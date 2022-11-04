429ac0f3c00182fe593c56c6b7207d8ec985bcc8
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-11-04 22:14:10 +0100

    sets and maps

--- arrays/setsNmaps.js
+++ arrays/setsNmaps.js
@@ -1,9 +1,9 @@
-const ids = new Set([1, 2, 3, 4]); // How to create a set
-console.log(ids.has(1));
+// const ids = new Set([1, 2, 3, 4]); // How to create a set
+// console.log(ids.has(1));
 
-for (const item of ids.entries()) {
-  console.log(item[0]);
-}
+// for (const item of ids.entries()) {
+//   console.log(item[0]);
+// }
 
 // MAPS & SETS
 // SET = data structure for storing (nested) data of any
@@ -16,20 +16,26 @@ for (const item of ids.entries()) {
 // It is iterable and also has some special methods in it.
 // It is ordered, Keys can't be dublicated, key based access.
 
-const person1 = { name: 'Chidozie' };
-const person2 = { name: 'Chukwuebuka' };
+// const person1 = { name: 'Chidozie' };
+// const person2 = { name: 'Chukwuebuka' };
 
-const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
-personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
-console.log(personData);
+// const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+// personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+// console.log(personData);
 
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
 
-for (const key of personData.keys()) {
-  console.log(key);
-}
-for (const value of personData.values()) {
-  console.log(value);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
+
+const person = { name: 'Chidozie' };
+const persons = new WeakSet();
+persons.add(person);
+
+console.log(persons);
